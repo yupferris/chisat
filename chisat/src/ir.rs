10799eb7d6740fdd6577e04ebff185057ca05a31
ferris <yupferris@gmail.com>
2021-12-02 22:53:32 +0100

    chisat: Add Context, remove Formula
    Also refactor arbitrary Context gen to use the Context's builder interface as a dogfooding exercise.

--- chisat/src/ir.rs
+++ chisat/src/ir.rs
@@ -3,6 +3,137 @@ use std::convert::identity;
 use std::fmt;
 use std::hash::Hash;
 
+// TODO: Rename?
+// TODO: Remove Clone
+#[derive(Clone)]
+pub struct Context {
+    pub(crate) clauses: Vec<Clause>,
+}
+
+impl Context {
+    pub fn new() -> Context {
+        Context {
+            clauses: Vec::new(),
+        }
+    }
+
+    // TODO: Rename?
+    pub fn clause(&mut self) -> ClauseBuilder<'_> {
+        ClauseBuilder {
+            context: self,
+            clause: None,
+        }
+    }
+
+    // TODO: This should mutate, not clone
+    pub(crate) fn assign(&self, variable: Variable, value: bool) -> Context {
+        Context {
+            clauses: self.clauses.iter().filter_map(|clause| {
+                if clause.literals.contains(&Literal {
+                    variable,
+                    is_positive: value,
+                }) {
+                    return None;
+                }
+
+                Some(Clause {
+                    literals: clause.literals.iter().copied().filter(|&literal| literal != Literal {
+                        variable,
+                        is_positive: !value,
+                    }).collect(),
+                })
+            }).collect(),
+        }
+    }
+
+    pub(crate) fn evaluate(&self, assignment: &Assignment) -> bool {
+        self.clauses.iter().map(|clause| clause.evaluate(assignment)).reduce(|a, b| a && b).unwrap_or(true)
+    }
+
+    pub(crate) fn first_pure_literal(&self) -> Option<Literal> {
+        let mut variable_map: HashMap<Variable, Option<Literal>> = HashMap::new();
+        for clause in &self.clauses {
+            for literal in &clause.literals {
+                let variable = literal.variable;
+                if let Some(prev_literal) = variable_map.get_mut(&variable) {
+                    if let Some(prev_literal_value) = prev_literal {
+                        if literal.is_positive != prev_literal_value.is_positive {
+                            *prev_literal = None;
+                        }
+                    }
+                } else {
+                    variable_map.insert(variable, Some(*literal));
+                }
+            }
+        }
+
+        variable_map.values().copied().find_map(identity)
+    }
+
+    pub(crate) fn first_unassigned_variable(&self, assignment: &Assignment) -> Option<Variable> {
+        self.clauses.iter().find_map(|clause| clause.literals.iter().find_map(|literal| {
+            let variable = literal.variable;
+            if !assignment.values.contains_key(&variable) {
+                Some(variable)
+            } else {
+                None
+            }
+        }))
+    }
+
+    pub(crate) fn first_unit_clause_literal(&self) -> Option<Literal> {
+        for clause in &self.clauses {
+            if clause.literals.len() == 1 {
+                return Some(clause.literals[0]);
+            }
+        }
+
+        None
+    }
+}
+
+// TODO: Does this still make sense?
+impl fmt::Debug for Context {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        writeln!(f)?;
+        for clause in &self.clauses {
+            writeln!(f, "{:?}", clause)?;
+        }
+
+        Ok(())
+    }
+}
+
+// TODO: Is this a sensible interface?
+pub struct ClauseBuilder<'a> {
+    context: &'a mut Context,
+    clause: Option<Clause>,
+}
+
+impl<'a> ClauseBuilder<'a> {
+    pub fn literal(&mut self, variable: Variable, is_positive: bool) {
+        let literal = Literal {
+            variable,
+            is_positive,
+        };
+        if let Some(clause) = &mut self.clause {
+            clause.literals.push(literal);
+        } else {
+            self.clause = Some(Clause {
+                literals: vec![literal],
+            });
+        }
+    }
+}
+
+impl<'a> Drop for ClauseBuilder<'a> {
+    fn drop(&mut self) {
+        if let Some(clause) = self.clause.take() {
+            self.context.clauses.push(clause);
+        }
+    }
+}
+
 #[derive(Clone, Copy, Eq, Hash, PartialEq)]
 #[repr(transparent)]
 pub struct Variable(u32);
@@ -66,89 +197,6 @@ impl fmt::Debug for Clause {
     }
 }
 
-#[derive(Clone)]
-pub struct Formula {
-    pub(crate) clauses: Vec<Clause>,
-}
-
-impl Formula {
-    pub(crate) fn assign(&self, variable: Variable, value: bool) -> Formula {
-        Formula {
-            clauses: self.clauses.iter().filter_map(|clause| {
-                if clause.literals.contains(&Literal {
-                    variable,
-                    is_positive: value,
-                }) {
-                    return None;
-                }
-
-                Some(Clause {
-                    literals: clause.literals.iter().copied().filter(|&literal| literal != Literal {
-                        variable,
-                        is_positive: !value,
-                    }).collect(),
-                })
-            }).collect(),
-        }
-    }
-
-    pub fn evaluate(&self, assignment: &Assignment) -> bool {
-        self.clauses.iter().map(|clause| clause.evaluate(assignment)).reduce(|a, b| a && b).unwrap_or(true)
-    }
-
-    pub(crate) fn first_pure_literal(&self) -> Option<Literal> {
-        let mut variable_map: HashMap<Variable, Option<Literal>> = HashMap::new();
-        for clause in &self.clauses {
-            for literal in &clause.literals {
-                let variable = literal.variable;
-                if let Some(prev_literal) = variable_map.get_mut(&variable) {
-                    if let Some(prev_literal_value) = prev_literal {
-                        if literal.is_positive != prev_literal_value.is_positive {
-                            *prev_literal = None;
-                        }
-                    }
-                } else {
-                    variable_map.insert(variable, Some(*literal));
-                }
-            }
-        }
-
-        variable_map.values().copied().find_map(identity)
-    }
-
-    pub(crate) fn first_unassigned_variable(&self, assignment: &Assignment) -> Option<Variable> {
-        self.clauses.iter().find_map(|clause| clause.literals.iter().find_map(|literal| {
-            let variable = literal.variable;
-            if !assignment.values.contains_key(&variable) {
-                Some(variable)
-            } else {
-                None
-            }
-        }))
-    }
-
-    pub(crate) fn first_unit_clause_literal(&self) -> Option<Literal> {
-        for clause in &self.clauses {
-            if clause.literals.len() == 1 {
-                return Some(clause.literals[0]);
-            }
-        }
-
-        None
-    }
-}
-
-impl fmt::Debug for Formula {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        writeln!(f)?;
-        for clause in &self.clauses {
-            writeln!(f, "{:?}", clause)?;
-        }
-
-        Ok(())
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct Assignment {
     pub values: HashMap<Variable, bool>,
@@ -174,43 +222,35 @@ mod tests {
 
     const ARBITRARY_NUM_VARIABLES: u32 = 8;
 
-    impl quickcheck::Arbitrary for Variable {
-        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
-            Variable(u32::arbitrary(g) % ARBITRARY_NUM_VARIABLES)
-        }
-    }
-
-    impl quickcheck::Arbitrary for Literal {
-        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
-            Literal {
-                variable: Variable::arbitrary(g),
-                is_positive: bool::arbitrary(g),
-            }
-        }
-    }
-
-    impl quickcheck::Arbitrary for Clause {
-        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
-            let max_num_literals = 4;
-            // TODO: This excludes 0, which typically results in more interesting formulas,
-            //  since any empty clause renders the entire formula unsatisfiable. However,
-            //  this may be an important case to check, so we may want to come up with a way
-            //  to conditionally include empty clauses with low probability of occurrence.
-            let num_literals = (u32::arbitrary(g) % max_num_literals) + 1;
-            Clause {
-                literals: (0..num_literals).map(|_| Literal::arbitrary(g)).collect(),
-            }
-        }
-    }
-
-    impl quickcheck::Arbitrary for Formula {
+    // TODO: Does this still make sense?
+    impl quickcheck::Arbitrary for Context {
         fn arbitrary(g: &mut quickcheck::Gen) -> Self {
             // TODO: Find a good way to respect size that doesn't end up generating too many
             //  unsatisfiable formulas
             let num_clauses = 4;//g.size();
-            Formula {
-                clauses: (0..num_clauses).map(|_| Clause::arbitrary(g)).collect(),
+            let mut ret = Context::new();
+            for _ in 0..num_clauses {
+                let mut clause = ret.clause();
+                let max_num_literals = 4;
+                // TODO: This excludes 0, which typically results in more interesting formulas,
+                //  since any empty clause renders the entire formula unsatisfiable. However,
+                //  this may be an important case to check, so we may want to come up with a way
+                //  to conditionally include empty clauses with low probability of occurrence.
+                let num_literals = (u32::arbitrary(g) % max_num_literals) + 1;
+                for _ in 0..num_literals {
+                    clause.literal(
+                        Variable::arbitrary(g),
+                        bool::arbitrary(g),
+                    );
+                }
             }
+            ret
+        }
+    }
+
+    impl quickcheck::Arbitrary for Variable {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            Variable(u32::arbitrary(g) % ARBITRARY_NUM_VARIABLES)
         }
     }
 }
--- chisat/src/solvers.rs
+++ chisat/src/solvers.rs
@@ -1,15 +1,15 @@
 use crate::ir::*;
 
-pub fn backtracking(formula: &Formula) -> (Option<Assignment>, u32) {
-    fn go(formula: &Formula, assignment: Assignment, num_search_steps: &mut u32) -> Option<Assignment> {
-        if formula.evaluate(&assignment) {
+pub fn backtracking(context: &Context) -> (Option<Assignment>, u32) {
+    fn go(context: &Context, assignment: Assignment, num_search_steps: &mut u32) -> Option<Assignment> {
+        if context.evaluate(&assignment) {
             return Some(assignment);
         }
-        if let Some(variable) = formula.first_unassigned_variable(&assignment) {
+        if let Some(variable) = context.first_unassigned_variable(&assignment) {
             for value in [false, true] {
                 *num_search_steps += 1;
                 let result = go(
-                    formula,
+                    context,
                     assignment.assign(variable, value),
                     num_search_steps,
                 );
@@ -21,43 +21,43 @@ pub fn backtracking(formula: &Formula) -> (Option<Assignment>, u32) {
         None
     }
     let mut num_search_steps = 0;
-    (go(formula, Assignment::empty(), &mut num_search_steps), num_search_steps)
+    (go(context, Assignment::empty(), &mut num_search_steps), num_search_steps)
 }
 
-pub fn dpll(formula: &Formula) -> (Option<Assignment>, u32) {
-    fn go(formula: &Formula, assignment: Assignment, num_search_steps: &mut u32) -> Option<Assignment> {
-        if formula.clauses.is_empty() {
+pub fn dpll(context: &Context) -> (Option<Assignment>, u32) {
+    fn go(context: &Context, assignment: Assignment, num_search_steps: &mut u32) -> Option<Assignment> {
+        if context.clauses.is_empty() {
             return Some(assignment);
         }
 
-        if formula.clauses.iter().any(|clause| clause.is_empty()) {
+        if context.clauses.iter().any(|clause| clause.is_empty()) {
             return None;
         }
 
         // Unit clause rule
-        if let Some(literal) = formula.first_unit_clause_literal() {
+        if let Some(literal) = context.first_unit_clause_literal() {
             return go(
-                &formula.assign(literal.variable, literal.is_positive),
+                &context.assign(literal.variable, literal.is_positive),
                 assignment.assign(literal.variable, literal.is_positive),
                 num_search_steps,
             );
         }
 
         // Pure literal rule
-        if let Some(literal) = formula.first_pure_literal() {
+        if let Some(literal) = context.first_pure_literal() {
             return go(
-                &formula.assign(literal.variable, literal.is_positive),
+                &context.assign(literal.variable, literal.is_positive),
                 assignment.assign(literal.variable, literal.is_positive),
                 num_search_steps,
             );
         }
 
         // Splitting rule
-        if let Some(variable) = formula.first_unassigned_variable(&assignment) {
+        if let Some(variable) = context.first_unassigned_variable(&assignment) {
             for value in [false, true] {
                 *num_search_steps += 1;
                 let result = go(
-                    &formula.assign(variable, value),
+                    &context.assign(variable, value),
                     assignment.assign(variable, value),
                     num_search_steps,
                 );
@@ -70,7 +70,7 @@ pub fn dpll(formula: &Formula) -> (Option<Assignment>, u32) {
         None
     }
     let mut num_search_steps = 0;
-    (go(formula, Assignment::empty(), &mut num_search_steps), num_search_steps)
+    (go(context, Assignment::empty(), &mut num_search_steps), num_search_steps)
 }
 
 #[cfg(test)]
@@ -78,32 +78,32 @@ mod tests {
     use super::*;
 
     #[quickcheck]
-    fn backtracking_satisfying_assignments_are_satisfying(formula: Formula) -> bool {
-        match backtracking(&formula).0 {
+    fn backtracking_satisfying_assignments_are_satisfying(context: Context) -> bool {
+        match backtracking(&context).0 {
             Some(assignment) => {
                 println!("Satisfying assignment: {:?}", assignment);
-                formula.evaluate(&assignment)
+                context.evaluate(&assignment)
             }
             _ => true,
         }
     }
 
     #[quickcheck]
-    fn dpll_satisfying_assignments_are_satisfying(formula: Formula) -> bool {
-        match dpll(&formula).0 {
+    fn dpll_satisfying_assignments_are_satisfying(context: Context) -> bool {
+        match dpll(&context).0 {
             Some(assignment) => {
                 println!("Satisfying assignment: {:?}", assignment);
-                formula.evaluate(&assignment)
+                context.evaluate(&assignment)
             }
             _ => true,
         }
     }
 
     #[quickcheck]
-    fn backtracking_and_dpll_reach_the_same_conclusion(formula: Formula) -> bool {
-        let backtracking_result = backtracking(&formula);
+    fn backtracking_and_dpll_reach_the_same_conclusion(context: Context) -> bool {
+        let backtracking_result = backtracking(&context);
         println!("backtracking result: {:?}", backtracking_result);
-        let dpll_result = dpll(&formula);
+        let dpll_result = dpll(&context);
         println!("dpll result: {:?}", dpll_result);
         let ret = backtracking_result.0.is_some() == dpll_result.0.is_some();
         println!();
@@ -111,10 +111,10 @@ mod tests {
     }
 
     #[quickcheck]
-    fn dpll_uses_the_same_or_fewer_search_steps_than_backtracking(formula: Formula) -> bool {
-        let backtracking_result = backtracking(&formula);
+    fn dpll_uses_the_same_or_fewer_search_steps_than_backtracking(context: Context) -> bool {
+        let backtracking_result = backtracking(&context);
         println!("backtracking result: {:?}", backtracking_result);
-        let dpll_result = dpll(&formula);
+        let dpll_result = dpll(&context);
         println!("dpll result: {:?}", dpll_result);
         dpll_result.1 <= backtracking_result.1
     }
