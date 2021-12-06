164a7a2df528c9ee35d1b679827c929f2e2ea235
ferris <yupferris@gmail.com>
2021-12-06 22:51:47 +0100

    chisat-cli: Accept multiple CNF file arguments

--- chisat-cli/src/main.rs
+++ chisat-cli/src/main.rs
@@ -6,43 +6,48 @@ use std::io;
 use std::time::Instant;
 
 fn main() -> io::Result<()> {
-    let cnf_file_name = env::args().skip(1).next().expect("Missing CNF file name arg");
-
-    let mut formula = Formula::new();
-
-    println!("c parsing file {}", cnf_file_name);
-    dimacs_cnf::parse(&mut formula, File::open(cnf_file_name)?)?;
-    println!("c parsing successful");
-
-    println!("c solving");
-    let start_time = Instant::now();
-    let result = solvers::dpll(&formula);
-    let elapsed_time = start_time.elapsed();
-    let elapsed_time_s = (elapsed_time.as_secs() as f64) + (elapsed_time.subsec_nanos() as f64) / 1000000000.0;
-    println!("c elapsed time: {:.*}s", 2, elapsed_time_s);
-    println!("c solving successful");
-    println!("c {} search steps", result.1);
-
-    match result.0 {
-        Some(assignment) => {
-            println!("s SATISFIABLE");
-            print!("v");
-            for (variable, value) in assignment.values {
-                print!(" ");
-                if !value {
-                    print!("-");
-                }
-                print!("{}", variable.index() as i32 + 1);
-            }
-            println!();
-        }
-        None => {
-            println!("s UNSATISFIABLE");
-            // TODO: Extract and print proof
-        }
+    if env::args().count() < 2 {
+        panic!("Missing CNF file name arg(s)");
     }
+    for cnf_file_name in env::args().skip(1) {
 
-    println!("c done");
+        let mut formula = Formula::new();
+
+        println!("c parsing file {}", cnf_file_name);
+        dimacs_cnf::parse(&mut formula, File::open(cnf_file_name)?)?;
+        println!("c parsing successful");
+
+        println!("c solving");
+        let start_time = Instant::now();
+        let result = solvers::dpll(&formula);
+        let elapsed_time = start_time.elapsed();
+        let elapsed_time_s = (elapsed_time.as_secs() as f64) + (elapsed_time.subsec_nanos() as f64) / 1000000000.0;
+        println!("c elapsed time: {:.*}s", 2, elapsed_time_s);
+        println!("c solving successful");
+        println!("c {} search steps", result.1);
+
+        match result.0 {
+            Some(assignment) => {
+                println!("s SATISFIABLE");
+                print!("v");
+                for (variable, value) in assignment.values {
+                    print!(" ");
+                    if !value {
+                        print!("-");
+                    }
+                    print!("{}", variable.index() as i32 + 1);
+                }
+                println!();
+            }
+            None => {
+                println!("s UNSATISFIABLE");
+                // TODO: Extract and print proof
+            }
+        }
+
+        println!("c done");
+        println!();
+    }
 
     Ok(())
 }
